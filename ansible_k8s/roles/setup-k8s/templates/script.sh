cef7ce4a4b5501fdb35443caa8c0402dbbc03d48
ridwansswnto <ridwan.siswnto@gmail.com>
2020-03-12 01:03:22 +0700

    ansible-k8s(roles): add install k8s

--- ansible_k8s/roles/setup-k8s/templates/script.sh
+++ ansible_k8s/roles/setup-k8s/templates/script.sh
@@ -2,6 +2,6 @@
 
 apt install curl
 curl -sSL https://get.docker.com/ | sh
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
-apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-apt update && apt install kubeadm
+# curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+# apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+# apt update && apt install kubeadm
